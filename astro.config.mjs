63356a8d85ef47308896c75c65b9440107122a4c
Yassine Doghri <yassine@adaures.com>
2024-02-10 17:20:43 +0000

    ci: add deploy workflow to publish Astro website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.yassinedoghri.com/",
   integrations: [
     tailwind(),
     icon({
